bf420254e73221d9b70d0e9bebd9aa3ce738ce50
Work <vanda70601s@mail.ru>
2023-10-31 00:10:43 +0300

    commit

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -5,14 +5,23 @@ public class Battleship8x8 {
     private long shots = 0L;
 
     public Battleship8x8(final long ships) {
+
         this.ships = ships;
     }
 
     public boolean shoot(String shot) {
-        throw new UnsupportedOperationException();
+        char [] chars = new char[shot.length()];
+        for (int i=0; i<shot.length(); i++) {
+            chars [i] =shot.charAt(i);
+        }
+int col = 0;
+        switch (chars [0]) {
+
+        }
     }
 
     public String state() {
+
         throw new UnsupportedOperationException();
     }
 }
